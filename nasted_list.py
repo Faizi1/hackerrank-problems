f24d9252c09a1659a619cb733780eb2f50534c59
faizi1 <faizsagheer4@gmail.com>
2023-08-17 02:08:18 +0500

    nasted_list program updated

--- nasted_list.py
+++ nasted_list.py
@@ -1,10 +1,36 @@
-# l = []
-# for _ in range(int(input())):
-#     name = input()
-#     score = float(input())
-#     l.append([name,score])
-# print(l)
-# for i in l:
-#     print(i)
+records = []
+score_list = []
+name_list = []
 
-print(18.0 // 4)
+
+#get name and score
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    records.append([name,score])
+
+print('records: ', records)
+
+#get score list
+for i in records:
+    name_list.append(i[0])
+    score_list.append(i[1])
+    
+sorted_score_list = sorted(score_list)
+print('sorted list: ', sorted_score_list)
+    
+sort_alphabetic = sorted(name_list)
+print('alphabatic sort: ', sort_alphabetic)
+
+for i in sorted_score_list:
+    if sorted_score_list.count(sorted_score_list[1]) > 1:
+        pass
+    
+for j in records:
+    if sorted_score_list[1] in j:
+        lowest_scorer = j[0]
+print('second lowest scorer name: ', lowest_scorer)
+
+
+
+
