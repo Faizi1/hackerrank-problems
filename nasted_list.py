33f9091ac70637bf727967c8fab5e4f0586590d8
faizi1 <faizsagheer4@gmail.com>
2023-08-17 17:24:10 +0500

    nested list program updated

--- nasted_list.py
+++ nasted_list.py
@@ -1,36 +1,59 @@
 records = []
-score_list = []
-name_list = []
 
-
-#get name and score
+# Get name and score
 for _ in range(int(input())):
     name = input()
     score = float(input())
-    records.append([name,score])
+    records.append([name, score])
 
-print('records: ', records)
+# Find the second lowest score
+second_lowest_score = sorted(set(score for name, score in records))[1]
 
-#get score list
-for i in records:
-    name_list.append(i[0])
-    score_list.append(i[1])
+# Get names with the second lowest score
+sorted_names = sorted(name for name, score in records if score == second_lowest_score)
+
+# Print the sorted names
+for name in sorted_names:
+    print(name)
+
+
+''' Test Code '''
+# records = []
+# score_list = []
+# name_list = []
+
+
+# #get name and score
+# for _ in range(int(input())):
+#     name = input()
+#     score = float(input())
+#     records.append([name,score])
+
+# print('records: ', records)
+
+# #get score list
+# for i in records:
+#     name_list.append(i[0])
+#     score_list.append(i[1])
+
+# print('name list: ', name_list)
+# print('score list: ', score_list)
     
-sorted_score_list = sorted(score_list)
-print('sorted list: ', sorted_score_list)
+# sorted_score_list = sorted(score_list)
+# print('sorted list: ', sorted_score_list)
     
-sort_alphabetic = sorted(name_list)
-print('alphabatic sort: ', sort_alphabetic)
-
-for i in sorted_score_list:
-    if sorted_score_list.count(sorted_score_list[1]) > 1:
-        pass
-    
-for j in records:
-    if sorted_score_list[1] in j:
-        lowest_scorer = j[0]
-print('second lowest scorer name: ', lowest_scorer)
-
+# sort_name = []
+# if sorted_score_list.count(sorted_score_list[1]) > 1:
+#     for i in records:
+#         if sorted_score_list[1] in i:
+#             sort_name.append(i[0])
+#     print('sort names: ', sort_name)
+#     for j in sorted(sort_name):
+#         print(j)
+# else:
+#     for j in records:
+#         if sorted_score_list[1] in j:
+#             print(j[0])
 
 
 
